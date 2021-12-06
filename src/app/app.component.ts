45066798aeac51c489ad0e63f3017092301c0936
Rajib Sahe <rajib.rs@gmail.com>
2021-12-06 22:29:30 +0530

    class 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-training';
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 }
